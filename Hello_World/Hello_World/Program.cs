4ce89e470328642f37b33086ba8b634b79fd0619
MarianKanev <mariankanev@gmail.com>
2022-11-26 20:47:56 +0200

    First commit

---
+++ Hello_World/Hello_World/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+ 
+namespace CSharpExamples {
+    class Program {
+        static void Main(string[] args) {
+            for(int i=3;i<=7;i++) {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
