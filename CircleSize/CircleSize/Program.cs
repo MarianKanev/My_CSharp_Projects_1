cd0920b4f8b61868fd8c12a94765cbfcea4925df
MarianKanev <mariankanev@gmail.com>
2022-11-30 20:10:05 +0200

    Circle Size

---
+++ CircleSize/CircleSize/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+namespace ConsoleApp001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Radius:");
+            double r = double.Parse(Console.ReadLine());
+            var S = 3.14 * r * r;
+            Console.Write("S = " +S);
+        }
+    }
+}
--- Simple_Calculator/Program.cs
+++ Simple_Calculator/Program.cs
@@ -49,17 +49,17 @@ namespace CalculatorApp {
         }  
         //Substraction  
         public static int Subtraction(int input_1, int input_2) {  
-            int result = input_1 + input_2;  
+            int result = input_1 - input_2;  
             return result;  
         }  
         //Multiplication  
         public static int Multiplication(int input_1, int input_2) {  
-            int result = input_1 + input_2;  
+            int result = input_1 * input_2;  
             return result;  
         }  
         //Division  
         public static int Division(int input_1, int input_2) {  
-            int result = input_1 + input_2;  
+            int result = input_1 / input_2;  
             return result;  
         }  
     }  
